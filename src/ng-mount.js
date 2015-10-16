b82f981ec8a1080f44bf25841ad5e858a74a2fe5
Marco Gagliardi <marcogagliardi84@gmail.com>
2015-10-16 10:54:15 +0200

    Check if init function exists

--- src/ng-mount.js
+++ src/ng-mount.js
@@ -1,4 +1,7 @@
 /**
+ *
+ * Author: Marco Gagliardi marcogagliardi84@gmail.com
+ *
  * General-purpose plugin loader. Bind any loading function
  * (typically of jQuery's plugins) to Angular's link function for
  * the plugins initialization on DOM elements.
@@ -15,6 +18,10 @@ angular.module('ngMount',[])
             // responsible for registering DOM listeners as well as updating the DOM
             link: function(scope, element, attrs) {
                 $timeout(function () {
+                    if (typeof $(element)[scope.$eval(attrs.ngMount).plugin] != "function") {
+                        console.error("ngMount error! (Function not found: "+scope.$eval(attrs.ngMount).plugin+")")
+                        return;
+                    }
                     $(element)[scope.$eval(attrs.ngMount).plugin](scope.$eval(attrs.ngMount).params);
                 });
             }
